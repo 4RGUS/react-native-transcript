de57ea66b236f1b53cc1b734853e2e150408f162
Vineeth Nambiar <vineeth.nambiar@neosoftmail.com>
2025-05-28 10:54:31 +0530

    feat: platform specific event emitter

--- src/NativeTranscription.ts
+++ src/NativeTranscription.ts
@@ -2,8 +2,10 @@ import {
   TurboModuleRegistry,
   NativeModules,
   DeviceEventEmitter,
+  Platform,
+  NativeEventEmitter,
 } from 'react-native';
-import type { TurboModule } from 'react-native';
+import type { DeviceEventEmitterStatic, TurboModule } from 'react-native';
 
 export interface Spec extends TurboModule {
   startListening(): void;
@@ -16,5 +18,12 @@ const Transcription: Spec =
   (TurboModuleRegistry.get<Spec>('Transcription') as Spec) ??
   (NativeModules.Transcription as Spec);
 
+type TransctiptionEventEmitterType =
+  | DeviceEventEmitterStatic
+  | NativeEventEmitter;
+
 export default Transcription;
-export const TranscriptionEventEmitter = DeviceEventEmitter;
+export const TranscriptionEventEmitter: TransctiptionEventEmitterType =
+  Platform.OS === 'android'
+    ? DeviceEventEmitter
+    : new NativeEventEmitter(NativeModules.Transcription);
