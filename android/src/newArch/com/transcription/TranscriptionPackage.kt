397c1685932a36339949074b19078d9be17d5812
Vineeth Nambiar <vntnambiar4@gmail.com>
2025-05-29 16:07:46 +0530

    fix: multiple package support

--- android/src/newArch/com/transcription/TranscriptionPackage.kt
+++ android/src/newArch/com/transcription/TranscriptionPackage.kt
@@ -6,26 +6,27 @@ import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.model.ReactModuleInfo
 import com.facebook.react.module.model.ReactModuleInfoProvider
-import java.util.HashMap
 
 class TranscriptionPackage : BaseReactPackage() {
+
   override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? {
     return if (name == TranscriptionModule.NAME) {
       TranscriptionModule(reactContext)
-    } else {
-      null
-    }
+    } else null
   }
 
   override fun getReactModuleInfoProvider(): ReactModuleInfoProvider {
     return ReactModuleInfoProvider {
-      val moduleInfos: MutableMap<String, ReactModuleInfo> = HashMap()
-      val isTurboModule: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+      val moduleInfos = mutableMapOf<String, ReactModuleInfo>()
+      val isTurboModule = true // Assuming new arch always uses TurboModules here
+
       moduleInfos[TranscriptionModule.NAME] = ReactModuleInfo(
-        TranscriptionModule.NAME, TranscriptionModule.NAME, canOverrideExistingModule = false,  // canOverrideExistingModule
+        TranscriptionModule.NAME,
+        TranscriptionModule.NAME,
+        canOverrideExistingModule = false,
         needsEagerInit = false,
         isCxxModule = false,
-        isTurboModule=isTurboModule
+        isTurboModule = isTurboModule
       )
       moduleInfos
     }
