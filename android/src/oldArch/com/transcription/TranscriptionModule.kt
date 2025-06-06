aafd7236679f2f054b760643ff5bddba27723969
Vineeth Nambiar <vntnambiar4@gmail.com>
2025-06-06 19:42:18 +0530

    fix: added imports

--- android/src/oldArch/com/transcription/TranscriptionModule.kt
+++ android/src/oldArch/com/transcription/TranscriptionModule.kt
@@ -1,9 +1,10 @@
 package com.transcription
 
+import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.Promise
+import com.facebook.react.bridge.Arguments
 
 class TranscriptionModule(private val reactContext: ReactApplicationContext) :
   ReactContextBaseJavaModule(reactContext) {
